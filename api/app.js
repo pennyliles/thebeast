9430a7b1fb55ee7928ec23009ac85a7e0db49573
frachen <59738945+frachen@users.noreply.github.com>
2022-10-26 17:30:59 -0400

    Revert "some changes"
    This reverts commit 9e6c5aa454dd052417861c4b526e9c2ff464b5cc.

--- api/app.js
+++ api/app.js
@@ -1,22 +1,14 @@
-const express = require('express')
-const app = express()
+const http = require('http');
 
-/* Routers */
-const routes = require("./routes")
+const hostname = '127.0.0.1';
+const port = 3000;
 
-const port = 3000
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-app.use('/', routes)
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-module.exports = app;
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
