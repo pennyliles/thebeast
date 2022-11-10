d5554e4bc8eb77d91143c2e76aba5ed3d5ecb0f4
frachen <59738945+frachen@users.noreply.github.com>
2022-11-10 18:54:38 -0500

    CRUD updates
    CRUD function updates

--- api/controllers/personLevelController.js
+++ api/controllers/personLevelController.js
@@ -1,4 +1,3 @@
-const express = require('express');
 const personLevelServices = require('../services/personLevelServices');
 
 async function getRecord(req, res) {
@@ -7,30 +6,45 @@ async function getRecord(req, res) {
         var result = await personLevelServices.getRecordFromDB(id);
         if (result) {
             res.status(200).jsonp(result);
+        } else {
+            res.status(200).json({ success: true, msg: 'Record not found.' })
         }
     } catch (e) {
         console.log(e.message);
-        res.sendStatus(500);
+        res.status(500).json({ success: false, msg: 'Failed to retrieve record.' });
     }
 }
 
 async function addRecord(req, res) {
     var body = req.body;
     try {
-        var exists = await personLevelServices.addRecordToDB(body);
-        if (exists) {
-            res.status(200).json({ sucess: true, msg: 'Record already exists' });
+        var status = await personLevelServices.addRecordToDB(body);
+        if (status) {
+            res.status(200).json({ success: true, msg: 'Record already exists.' });
         } else {
-            res.status(200).json({ sucess: true, msg: 'Record added.' });
+            res.status(200).json({ success: true, msg: 'Record added.' });
         }
     } catch (e) {
         console.log(e.message);
-        res.sendStatus(500).json({ sucess: false, msg: 'Failed to add record.' });
+        res.status(500).json({ success: false, msg: 'Failed to add record.' });
     }
 }
 
 async function updateRecord(req, res) {
+    var id = req.params.id;
+    var body = req.body;
 
+    try {
+        var status = await personLevelServices.updateRecordInDB(id, body);
+        if (status) {
+            res.status(200).json({ success: true, msg: 'Successfully updated the record.' });
+        } else {
+            res.status(200).json({ success: true, msg: 'Record not found.' });
+        }
+    } catch (e) {
+        console.log(e.message);
+        res.status(500).json({ success: false, msg: 'Failed to update record.' });
+    }
 }
 
 async function deleteRecord(req, res) {
@@ -38,13 +52,13 @@ async function deleteRecord(req, res) {
     try {
         var status = await personLevelServices.deleteRecordFromDB(id);
         if (status) {
-            res.status(200).json({ sucess: true, msg: 'Record deleted' });
+            res.status(200).json({ sucess: true, msg: 'Record deleted.' });
         } else {
-            res.status(200).json({ sucess: true, msg: 'Record not found or deleted.' });
+            res.status(200).json({ sucess: true, msg: 'Record not found.' });
         }
     } catch (e) {
         console.log(e.message);
-        res.sendStatus(500);
+        res.status(500).json({ success: false, msg: 'Failed to delete record.' });
     }
 }
 
--- api/services/personLevelServices.js
+++ api/services/personLevelServices.js
@@ -1,34 +1,79 @@
 const personLevelModel = require('../models/personLevelSchema');
 
 async function getRecordFromDB(id) {
-    var record = await personLevelModel.find({ uid: id });
+    var record = await personLevelModel.findOne({ uid: id }, function (err, doc) {
+        if (err) {
+            throw err;
+        } else {
+            if (doc) {
+                console.log('Found ' + doc);
+            } else {
+                console.log('Could not find record with uid: ' + id);
+            }
+        }
+    }).clone();
+
     return record;
 }
 
 async function addRecordToDB(body) {
     var record = new personLevelModel(body);
-    var status = await personLevelModel.find(body);
+    var status = await personLevelModel.findOne(body, function (err, doc) {
+        if (err) {
+            throw err;
+        } else {
+            if (doc) {
+                console.log('Record already exists.');
+            } else {
+                record.save(function (err, doc) {
+                    if (err) {
+                        throw err;
+                    } else {
+                        console.log('Added ' + doc);
+                    }
+                });
+            }
+        }
+    }).clone();
 
-    if (!status.length) {
-        record.save();
-        return false;
-    } else {
-        return true;
-    }
+    return status;
+}
+
+async function updateRecordInDB(id, body) {
+    var status = await personLevelModel.findOneAndUpdate({ uid: id }, body, function (err, doc) {
+        if (err) {
+            throw err;
+        } else {
+            if (doc) {
+                console.log('Sucessfully updated the record to: ' + doc);
+            } else {
+                console.log('No record found to update.');
+            }
+        }
+    }).clone();
+
+    return status;
 }
 
 async function deleteRecordFromDB(id) {
-    var status = await personLevelModel.findOneAndDelete({ uid: id });
+    var status = await personLevelModel.findOneAndDelete({ uid: id }, function (err, doc) {
+        if (err) {
+            throw err;
+        } else {
+            if (doc) {
+                console.log('Sucessfully deleted record :' + doc);
+            } else {
+                console.log('No record found to delete.');
+            }
+        }
+    }).clone();
 
-    if (!status.length) {
-        return true;
-    } else {
-        return false;
-    }
+    return status;
 }
 
 module.exports = {
     getRecordFromDB,
     addRecordToDB,
+    updateRecordInDB,
     deleteRecordFromDB
 };
