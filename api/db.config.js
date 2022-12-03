abaca0e6e0e474091bc7f09b028045f148738ee8
EliHedrick <54414439+EliHedrick@users.noreply.github.com>
2022-12-03 16:27:40 -0500

    navbar and homepage
    navbar button redesign and slight homepage change

--- api/db.config.js
+++ api/db.config.js
@@ -2,4 +2,5 @@
 module.exports = {
     url:
         "mongodb+srv://ac22:thebeast@thebeastdatabase.dfi9k2p.mongodb.net/thebeast?retryWrites=true&w=majority"
+        //"mongodb://0.0.0.0:27017" //for use localhost testing on windows
 };
