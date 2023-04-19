e92ff3979c2c7796aa32a44b43742df3b1880351
frachen <59738945+frachen@users.noreply.github.com>
2023-04-18 20:12:18 -0400

    Update db.config.js

--- api/db.config.js
+++ api/db.config.js
@@ -1,7 +1,7 @@
 /* Exporting MongoDB URI */
 module.exports = {
     url:
-        "mongodb+srv://ac22:thebeast@thebeastdatabase.dfi9k2p.mongodb.net/thebeast?retryWrites=true&w=majority"
+        "placeholder"
     /* mongodb+srv://<user>:<pass>@<database-name>.dfi9k2p.mongodb.net/thebeast?retryWrites=true&w=majority */
     //"mongodb://0.0.0.0:27017" //for use localhost testing on windows
 };
